9210e341dbe40f1fbe3d2c18bc8ad51f618011ca
Harry Maclean <harrymaclean@gocardless.com>
2018-04-22 15:58:08 +0100

    Use Map for state machine in tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -13,28 +13,28 @@ data Command a b =
   | Set a b
   deriving (Eq, Show)
 
-newtype StateMachine a b = StateMachine { smMap :: [(a, b)] }
+newtype StateMachine a b = StateMachine { smMap :: Map.Map a b }
   deriving (Eq, Show)
 
 type StateMachineM a b = StateT (StateMachine a b) Identity
 
-apply :: Command a b -> StateMachineM a b ()
+apply :: Ord a => Command a b -> StateMachineM a b ()
 apply NoOp = pure ()
 apply (Set k v) = do
   machineMap <- gets smMap
-  (put . StateMachine) $ (k, v) : machineMap
+  (put . StateMachine) $ Map.insert k v machineMap
 
 main :: IO ()
 main = do
   let s0 :: (ServerState (Command String Int), StateMachine String Int)
-      s0 = mkServer 0 [1, 2] 3
+      s0 = mkServer 0 [1, 2] 1
       s1 = mkServer 1 [0, 2] 3
-      s2 = mkServer 2 [0, 1] 3
+      s2 = mkServer 2 [0, 1] 4
       servers = Map.insert 2 s2 $ Map.insert 1 s1 $ Map.insert 0 s0 Map.empty
   testLoop servers
 
 testLoop :: Map.Map ServerId (ServerState (Command String Int), StateMachine String Int) -> IO ()
-testLoop s = go s [Tick 0, Tick 0, Tick 0, Tick 0, Tick 0, ClientRequest 0 (Set "foo" 42)]
+testLoop s = go s [Tick 0, Tick 0, ClientRequest 0 (Set "foo" 42), ClientRequest 1 (Set "bar" 43), ClientRequest 0 (Set "foo" 7)]
   where go :: Map.Map ServerId (ServerState (Command String Int), StateMachine String Int) -> [Message (Command String Int)] -> IO ()
         go servers [] = putStrLn "finished."
         go servers (msg:queue) = do
@@ -54,7 +54,7 @@ testLoop s = go s [Tick 0, Tick 0, Tick 0, Tick 0, Tick 0, ClientRequest 0 (Set 
           go servers' (queue ++ msgs)
 
 mkServer :: Int -> [ServerId] -> Int -> (ServerState (Command a b), StateMachine a b)
-mkServer serverId otherServerIds electionTimeout = (serverState, StateMachine { smMap = [] })
+mkServer serverId otherServerIds electionTimeout = (serverState, StateMachine { smMap = Map.empty })
   where t0 = Term { unTerm = 0 }
         initialMap :: Map.Map ServerId Int
         initialMap = foldl' (\m sid -> Map.insert sid 0 m) Map.empty otherServerIds
